097c06cffd627d87fcbc5f474b1aa29fc9145cc1
ismailaydemir <67390681+ismailaydemirx@users.noreply.github.com>
2025-05-15 17:05:44 +0300

    add: UBL test with xml serializer

--- FaturaXMLTest/obj/Debug/net9.0/FaturaXMLTest.AssemblyInfo.cs
+++ FaturaXMLTest/obj/Debug/net9.0/FaturaXMLTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FaturaXMLTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfc3866e3df14316089111e983ca275ef2acf0e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("FaturaXMLTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FaturaXMLTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ UBLSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using UblSharp;
+
+class Program
+{
+    static void Main()
+    {
+        var xmlPath = "../../../invoice.xml";
+
+        using (var stream = File.OpenRead(xmlPath))
+        {
+            var serializer = new XmlSerializer(typeof(InvoiceType));
+            var invoice = (InvoiceType)serializer.Deserialize(stream);
+
+            var faturaId = invoice.ID.Value;
+            var faturaTarihi = invoice.IssueDate.Value;
+            var Tedarikci = invoice.AccountingSupplierParty.Party.PartyName[0].Name.Value;
+            var toplamTutar = invoice.LegalMonetaryTotal.PayableAmount.Value;
+            var paraBirimi = invoice.LegalMonetaryTotal.PayableAmount.currencyID;
+
+            Console.WriteLine($"Fatura ID: {faturaId}");
+            Console.WriteLine($"Fatura Tarihi: {faturaTarihi}");
+            Console.WriteLine($"Tedarikçi: {Tedarikci}");
+            Console.WriteLine($"Toplam Tutar: {toplamTutar} {paraBirimi}");
+        }
+    }
+}
